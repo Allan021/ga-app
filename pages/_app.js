a0724deb374d0adc8a6c33eb32f937a12f1dfe06
acastro4 <acastro@signscloud.com>
2022-09-20 20:43:42 -0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-
+import "../styles/globals.css";
+import Script from "next/script";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-1YS00D1GXD"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || []; 
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-1YS00D1GXD',{ 'optimize_id':'OPT-524RCFP' });
+           `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
