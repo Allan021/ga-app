ed0eba364bccb5689bc07778255b6c7cefe8cb3c
acastro4 <acastro@signscloud.com>
2022-09-20 21:54:30 -0600

    update google analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,7 @@
 import "../styles/globals.css";
-import Script from "next/script";
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-59PMHCJ5SJ"
-      ></Script>
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-            window.dataLayer = window.dataLayer || []; 
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-            
-           gtag('config', 'G-59PMHCJ5SJ',{ 'optimize_id':'OPT-NWFQK7L' });
-           `}
-      </Script>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
